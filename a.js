4e0f8983d9921cb384be43cb26963b553a2df5bf
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2021-11-25 21:22:33 +0900

    승연이가 하다 만 것

--- a.js
+++ a.js
@@ -39,3 +39,53 @@ function a(){
     var num2=txt2.value;    
     
 }
+
+function loading(){
+    console.log("hey");  
+    // var none = document.getElementById('table')
+
+
+    var num1=txt1.value;         // txt1의 값을 num1에 초기화 시킨다.
+    var num2=txt2.value;         // txt2의 값을 num2에 초기화 시킨다. 
+    var count=1;
+    for(var i=0;i<num1;i++){
+     var trObj=document.createElement("tr");     //txt1에 입력된 값만큼 for문을 돌려 tr태그를 생성
+     for(var j=0;j<2;j++){
+      var tdObj=document.createElement("td");  //txt2에 입력된 값만큼 for문을 돌려 td태그를 생성
+      trObj.appendChild(tdObj);
+      tdObj.innerHTML=count;                 //생성된 td에 count를 innerHTML로 집어 넣는다.
+      count++;
+      trObj.style.background = "pink"  ;                          //count를  for문을 돌리는 만큼 증가시킨다. 
+     }
+     divObj.appendChild(trObj);    //div 태그에 tr태그를 붙여넣는다.
+    }  
+
+    for(var i=0;i<num1;i++){
+            var newForm = document.createElement('form');
+
+            newForm.name = i;
+            newForm.method = 'post';
+            newForm,action = 'https://127.';
+            newForm.target = '_blank';
+
+            var input1 = document.createElement('input');
+
+            input1.setAttribute("type","hidden");
+            input1.setAttribute("name","data1");
+            input1.setAttribute("value","value1");
+
+            newForm.appendChild(input);
+
+            document.body.appendChild(newForm);
+
+            newForm.submit();
+
+
+
+        }                          //count를  for문을 돌리는 만큼 증가시킨다. 
+
+    }
+
+   
+
+   
